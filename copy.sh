2681337ef7eb8a6e719da38d3320e31244904f0c
Garlicsauce <adrian.michalik@wp.pl>
2016-10-24 20:40:50 +0200

    1st version of script

---
+++ copy.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+HELP="UŻYCIE:\n\tkopiuj DESTINATION SOURCE...\nOPIS:\n\tKopiuje pliki oraz foldery (SOURCE) do DEST, na końcu każdego argumentu (SOURCE) plikowego dodaje suffix .userName, każdy argument będący folderem pakuje do archiwum .tar, dodając do niego suffix .userName.tar"
+
+if [ "$#" -lt 2 ]
+then
+	echo $HELP
+	echo "Błąd - zbyt mała liczba argumentów!"
+	exit 1
+fi
+
+if ! [ -d "$1" ]
+then
+	echo $HELP
+	echo "Błąd - pierwszy argument musi być folderem!"
+	exit 1
+fi
+
+for var in "$@"
+do
+	if [ -d "$var" ]
+	then
+		echo "$var is a directory"
+	elif [ -f "$var" ]
+	then
+		echo "$var is a file"
+	else
+		echo "$var is invalid"
+	fi
+done
