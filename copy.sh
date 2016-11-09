ab81df2e0cca3d936f1e9f9a17bb9019a19d0b4c
Garlicsauce <adrian.michalik@wp.pl>
2016-11-09 21:25:00 +0100

    working script

--- copy.sh
+++ copy.sh
@@ -4,31 +4,39 @@ HELP="UŻYCIE:\n\tkopiuj DESTINATION SOURCE...\nOPIS:\n\tKopiuje pliki oraz fold
 
 if [ "$#" -lt 2 ]
 then
-	echo $HELP
+	echo ${HELP}
 	echo "Błąd - zbyt mała liczba argumentów!"
 	exit 1
 fi
 
-if ! [ -d "$1" ]
+if [ -e "$1" -a -f "$1" ]
 then
-	echo $HELP
-	echo "Błąd - pierwszy argument musi być folderem!"
+	echo ${HELP}
+	echo "Błąd - pierwszy argument nie może być plikiem!"
 	exit 1
+elif ! [ -e "$1" ]
+then
+    mkdir $1
 fi
 
 DEST="$1"
+TAR_EXT=".tar"
+FILE_SUFFIX=".${USER}"
+TAR_DIR_SUFFIX=".${USER}${TAR_EXT}"
 
 for var in "$@"
 do
-	if [ -d "$var" ]
+	if [ -d "${var}" -a "${var}" != "${DEST}" ]
 	then
-		echo "$var is a directory"
+		tar -czvf ${var}${TAR_DIR_SUFFIX} ${var}
+		cp -r ${var}${TAR_DIR_SUFFIX} ${DEST}/${var}${TAR_DIR_SUFFIX}
+		rm ${var}${TAR_DIR_SUFFIX}
 	elif [ -f "$var" ]
 	then
-		echo "$var is a file"
-		echo "$var will be copied to $DEST"
-		cp $var $DEST
+		cp ${var} ${DEST}
+		mv ${DEST}/${var} ${DEST}/${var}${FILE_SUFFIX}
 	else
-		echo "$var is invalid"
+		[ "${var}" != "$DEST" ] && echo "$var is invalid"
 	fi
 done
+exit 0
